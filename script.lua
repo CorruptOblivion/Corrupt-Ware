6e63f9cb2df5e41b6625b27d2dc349b257114812
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-11-05 11:57:34 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,23 @@
-game:GetService('Players').LocalPlayer:Kick('Not done yet XD')
+repeat task.wait() until game:IsLoaded()
+
+function load(a)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/CorruptOblivion/Corrupt-Ware/main/Games/'..a..'.lua'))()
+end
+
+function notify(a,b,c,d)
+    game:GetService('StarterGui'):SetCore('SendNotification', {
+        Title = a,
+        Text = b,
+        Duration = c,
+        Icon = d
+    })
+end
+
+local PlaceId = game.PlaceId
+
+if PlaceId == 11237748076 then
+    load('StrangerThingsDay')
+    notify('CorruptWare','Loading script',5)
+else
+    notify('CorruptWare','Game Not Supported',25)
+end
