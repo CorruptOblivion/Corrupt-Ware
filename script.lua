f4236293c268f6e79f24c414b98c126bb9a55804
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-11-05 17:15:17 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -20,6 +20,9 @@ local PlaceId = game.PlaceId
 if PlaceId == 11237748076 then
     load('Games', 'StrangerThingsDay')
     notify('CorruptWare','Loading script',5)
+elseif PlaceId == 11436221007 then
+    load('Games', 'BeyondTheYellowBrickRoad')
+    notify('CorruptWare','Loading script',5)
 else
     notify('CorruptWare','Game Not Supported',25)
 end
