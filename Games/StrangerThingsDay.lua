5f7cbb33f10f4266ba21a951fd847486ab538414
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-11-05 18:39:02 -0700

    Update StrangerThingsDay.lua

--- Games/StrangerThingsDay.lua
+++ Games/StrangerThingsDay.lua
@@ -1,1 +1,10 @@
+function notify(a,b,c,d)
+    game:GetService('StarterGui'):SetCore('SendNotification', {
+        Title = a,
+        Text = b,
+        Duration = c,
+        Icon = d
+    })
+end
 
+notify('CorruptWare', 'Coming Soon', 5, nil)
