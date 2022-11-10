de0254c3996273c4cf1fd1841a8b5de0a1938fdf
CorruptOblivion <98083486+CorruptOblivion@users.noreply.github.com>
2022-11-10 14:38:32 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -16,13 +16,19 @@ end
 task.spawn(function() task.wait(3) load('ExtraScripts', 'DiscordInvite') end)
 
 local PlaceId = game.PlaceId
+local GameName = game:GetService('MarketplaceService'):GetProductInfo(PlaceId).Name
+
+local TemplateName = string.format('Loading Script - %s', GameName)
 
 if PlaceId == 11237748076 then
-    notify('CorruptWare','Loading script',5)
+    notify('CorruptWare', TemplateName, 5)
     load('Games', 'StrangerThingsDay')
 elseif PlaceId == 11436221007 then
-    notify('CorruptWare','Loading script',5)
+    notify('CorruptWare', TemplateName, 5)
     load('Games', 'BeyondTheYellowBrickRoad')
+elseif PlaceId == 8209480473 then
+    notify('CorruptWare', TemplateName, 5)
+    load('Games', 'SpotifyIsland')
 else
     notify('CorruptWare','Game Not Supported',25)
 end
